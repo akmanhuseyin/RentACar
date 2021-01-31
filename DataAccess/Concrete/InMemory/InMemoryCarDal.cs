a0be511a6eb5739ab435845369c09ca17d83f2a8
HSA <artemishsa@gmail.com>
2021-01-31 20:52:46 +0300

    My First Commit

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2021,DailyPrice=900000, Description="BMW 5 Series"},
+                new Car{Id=2,BrandId=2,ColorId=2,ModelYear=2017,DailyPrice=800000, Description="BMW 3 Series"},
+                new Car{Id=3,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=600000, Description="BMW 1 Series"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.Find(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return (Car)_cars.Find(c => c.Id == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.Find(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
