64b983349af184d93e0da615fdf367981060bc96
HSA <artemishsa@gmail.com>
2021-02-13 14:01:39 +0300

    Rental, User, Customer implemented.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,26 +10,47 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            //customerManager.Add(new Entities.Concrete.Customer() { CompanyName = "SunExpress" });
+
+
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            //var addResult = rentalManager.Add(new Rental() { CarId = 3, CustomerId = 1, RentDate = DateTime.Now });
+
+            //if (addResult.Success)
+            //{
+            //    foreach (var item in rentalManager.GetAll().Data)
+            //    {
+            //        Console.WriteLine(item.CarId + "\t --- \t" + item.CustomerId);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(addResult.Message);
+            //}
+            rentalManager.GetById(1).Data.
+
+
             //CarTest();
 
             //ColorTest();
 
             //BrandTest();
 
-            CarManager carManager = new CarManager(new EfCarDal());
-            var result = carManager.GetCarDetail();
-            if (result.Success)
-            {
-                foreach (var item in result.Data)
-                {
-                    Console.WriteLine("{0} *** {1} *** {2}", item.BrandName, item.ColorName, item.Description);
-                }
-            }
-            else
-            {
-                Console.WriteLine(result.Message);
-            }
-            
+            //CarManager carManager = new CarManager(new EfCarDal());
+            //var result = carManager.GetCarDetail();
+            //if (result.Success)
+            //{
+            //    foreach (var item in result.Data)
+            //    {
+            //        Console.WriteLine("{0} *** {1} *** {2}", item.BrandName, item.ColorName, item.Description);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result.Message);
+            //}
+
         }
 
         private static void BrandTest()
