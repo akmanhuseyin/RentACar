52737a55fbf4c85a4faec5fec2393dc851599d43
HSA <artemishsa@gmail.com>
2021-02-26 10:10:51 +0300

    Validators implemented.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).NotEmpty().GreaterThan(0);
+            RuleFor(c => c.ModelYear).NotEmpty().GreaterThan(2000).LessThan(DateTime.Now.Year).WithMessage("Arabalar 2000 ile "+ DateTime.Now.Year + " model arası olmalıdır!" );
+            RuleFor(c => c.Description).NotEmpty().WithMessage("Açıklama zorunludur!");
+            RuleFor(c => c.Description).MinimumLength(3);
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty().MinimumLength(2);
+            RuleFor(u => u.LastName).NotEmpty().MinimumLength(2);
+            RuleFor(u => u.Password).NotEmpty().MinimumLength(6);
+            RuleFor(u => u.Email).NotEmpty().MinimumLength(5).Must(HaveToIncludeChar).WithMessage("@ karakteri içermeyen email girdiniz!");
+        }
+
+        private bool HaveToIncludeChar(string arg)
+        {
+            return arg.Contains("@");
+        }
+    }
+}
